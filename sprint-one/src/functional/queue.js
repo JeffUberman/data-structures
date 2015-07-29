1396e903fe83eee34ff13cb9624b9a1ec77cae29
MKS-students <mks.students@hackreactor.com>
2015-07-29 16:49:42 -0500

    Completed functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    for(var i = size; i>0; i--){
+      storage[i] = storage[i-1];
+    }
+    storage[0] = value;
+
+    size ++;
   };
 
   someInstance.dequeue = function(){
+    var temp = storage[size-1];
+    if(size === 0){
+      return 0;
+    }
+    delete storage[size-1];
+    size--;
+    return temp;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size ++;
   };
 
   someInstance.pop = function(){
+    if(size<1){
+      return
+    }
+
+    var temp = storage[size-1];
+    delete storage[size-1];
+    size --;
+    return temp;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
