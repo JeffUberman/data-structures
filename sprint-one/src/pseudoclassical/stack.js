ddca1804eafdaabd24f955072d8fc4df8d84d227
MKS-students <mks.students@hackreactor.com>
2015-07-30 11:45:55 -0500

    Completed pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
 
-
+Stack.prototype.push = function(val) {
+  this.storage[this.counter] =  val;
+  this.counter ++;
+};
+Stack.prototype.pop = function() {
+  if(this.counter<1){
+    return 0;
+  }
+  var temp = this.storage[this.counter - 1];
+  delete this.storage[this.counter -1];
+  this.counter --;
+  return temp;
+};
+Stack.prototype.size = function() {
+  return this.counter;
+};
