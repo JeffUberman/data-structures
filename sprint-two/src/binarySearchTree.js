80f19d70eac5cbb9f7ecad476dc9685e03e01f5b
MKS-students <mks.students@hackreactor.com>
2015-08-01 17:29:59 -0500

    mostly complete binarysearchtree and hashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,76 @@
 var BinarySearchTree = function(value){
+  var obj = Object.create(sharedMethods);
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
 
+var sharedMethods = {
+
+  insert : function(node){
+    if(node > this.value){
+      if(this.right === null){
+        this.right = BinarySearchTree(node);
+      } else {
+        this.right.insert(node);
+      }
+    }
+
+
+
+    else if(node < this.value){
+      if(this.left === null){
+        this.left = BinarySearchTree(node);
+      } else {
+        this.left.insert(node);
+      }
+    }
+
+  },
+
+  contains : function(target){
+    if(this.value === target){
+      return true;
+    }
+    else if(target<this.value){
+      if(this.left ===null){
+        return;
+      }else{
+        return this.left.contains(target);
+      }
+    }
+
+    else if(target>this.value){
+      if(this.right === null){
+        return;
+      }else{
+        return this.right.contains(target);
+      }
+    }
+
+
+    return false
+  },
+
+  depthFirstLog : function(cb){
+
+    cb(this.value)
+
+    if(this.left === null){
+      return;
+    }else{
+      this.left.depthFirstLog(cb);
+    }
+
+    if(this.right === null){
+      return;
+    }else{
+      this.right.depthFirstLog(cb);
+    }
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,26 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(i);
+
+    this._storage.set(i, [k,v]);
+
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var tuple = this._storage.get(i);
+  if(!tuple){
+    return null;
+  }
+  return tuple[1]
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 };
 
 
