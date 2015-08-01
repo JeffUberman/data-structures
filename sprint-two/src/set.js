801833905c76297aff80e40deec1a108b445379f
MKS-students <mks.students@hackreactor.com>
2015-07-31 20:00:14 -0500

    Completed Set, need to make one more test pass for tree

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+
+ for(var i =0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      this._storage.splice(i, 1)
+    }
+  }
+
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value){
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,14 +16,58 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  var found = false;
+
+  var nodeChecker = function(node){
+    if(node.value === target){
+      found = true;
+    }
+    if(node.children){
+      for(var i = 0; i < node.children.length; i++){
+        return nodeChecker(node.children[i]);
+      }
+    }
+
+     return found;
+
+  };
+
+   return nodeChecker(this);
 
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+----------
+var tree1 = Tree(1)
+
+{
+  value : value;
+  addChild : function(value){}
+  chidren: []
+}
+
+tree1.addChild('H')
+tree1.children[0].addChildren("I")
+
+contains -->
+
+//iterate over the array
+
+  //object at index's value
+
+  //does it's nested array have children
+     //if yes recurse
+*/
+
+
+
+
+
+
+
